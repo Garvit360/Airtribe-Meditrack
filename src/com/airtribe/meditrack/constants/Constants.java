c5a3cf399cd4ea15fea2ca2d29da576bcdf67121
Garvit360 <dhakatesahil003@gmail.com>
2026-07-14 18:48:37 +0530

    Add CSV persistence utility

--- src/com/airtribe/meditrack/constants/Constants.java
+++ src/com/airtribe/meditrack/constants/Constants.java
@@ -10,6 +10,11 @@ public class Constants {
     public static final String DATE_FORMAT = "yyyy-MM-dd";
     public static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm";
 
+    public static final String DATA_DIR = "data";
+    public static final String PATIENTS_FILE = DATA_DIR + "/patients.csv";
+    public static final String DOCTORS_FILE = DATA_DIR + "/doctors.csv";
+    public static final String APPOINTMENTS_FILE = DATA_DIR + "/appointments.csv";
+
     private Constants(){
         // private constructor to prevent instantiations
     }
